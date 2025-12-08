2c96eb6a136dfe61ad4cd311e3e1a4634769bbe4
jui1960 <juifull196044@gmail.com>
2025-12-08 13:37:26 +0600

    building race

---
+++ Building_Race.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        double a, b, x, y;
+        cin >> a >> b >> x>> y;
+        double chef = a / b;
+        double chefina = x / y;
+        if (chef == chefina)
+        {
+            cout << "Both\n";
+        }
+        else if (chef < chefina)
+        {
+            cout << "Chef\n";
+        }
+        else
+        {
+            cout << "Chefina\n";
+        }
+        // double chef = (double)a / x ;
+        // if (== (double)b / y)
+        //     cout << "Both\n";
+        // else if ((double)a / x < (double)b / y)
+        //     cout << "Chef\n";
+        // else
+        //     cout << "Chefina\n";
+    }
+
+    return 0;
+}

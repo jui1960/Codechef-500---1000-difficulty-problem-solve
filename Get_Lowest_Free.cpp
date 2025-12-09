7fcf8253cb7e187ae9879eaa2736f8b0165abf91
jui1960 <juifull196044@gmail.com>
2025-12-09 17:50:49 +0600

    get lowest free

---
+++ Get_Lowest_Free.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        int sum = a + b + c;
+        int ans = 0;
+
+        if (a <= b && a <= c)
+        {
+            ans = sum - a;
+            cout << ans << '\n';
+        }
+        else if (b <= c)
+        {
+            ans = sum - b;
+            cout << ans << '\n';
+        }
+        else
+        {
+            ans = sum - c;
+            cout << ans << '\n';
+        }
+    }
+
+    return 0;
+}
--- Single_use_Attack.cpp
+++ Single_use_Attack.cpp
@@ -1,19 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
-    cin>>t;
+    cin >> t;
     while (t--)
     {
-        int x,y,h;
-        cin>>x>>y>>h;
-    }
-    
+        int x, y, h;
+        cin >> x >> y >> h;
+        int cnt, ans = 0;
+        if (x == h)
+            cout << 1 << '\n';
+        else
+        {
+            cnt = x - h;
+            ans = cnt / y;
+            if (cnt % y != 0)
+            {
+                ans += 2;
+                cout << ans << "\n";
+            }
+            else
+            {
+                ans += 1;
+                cout << ans << '\n';
+            }
 
-    
+            // if(x%y==0)
+            // cout<<cnt<<'\n';
+        }
+    }
 
     return 0;
 }

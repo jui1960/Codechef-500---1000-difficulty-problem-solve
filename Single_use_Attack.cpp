111a9e0d2f41c42e4dd0632f554633cbc969c133
jui1960 <juifull196044@gmail.com>
2025-12-09 12:48:57 +0600

    single use attack

---
+++ Single_use_Attack.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int x,y,h;
+        cin>>x>>y>>h;
+    }
+    
+
+    
+
+    return 0;
+}

d03440bab1630dc658d2a2da71e57d86ec66a260
jui1960 <juifull196044@gmail.com>
2025-12-14 18:14:03 +0600

    minimum carr

--- Minimum_Cars_required.cpp
+++ Minimum_Cars_required.cpp
@@ -1,14 +1,44 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	// your code goes here
+// 	int t;
+// 	cin>>t;
+// 	while(t--){
+// 	    int n;
+// 	    cin>>n;
+// 	    cout<<(n+3)/4<<'\n';
+// 	}
+
+// }
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
 	// your code goes here
-	int t;
-	cin>>t;
-	while(t--){
-	    int n;
-	    cin>>n;
-	    cout<<(n+3)/4<<'\n';
-	}
-
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int ans = n/4;
+        if(n%4!=0) 
+        {
+            ans++;
+            cout<<ans<<'\n';
+           
+        }
+        else{
+            cout<<ans<<'\n';
+        }
+        
+        
+    }
 }
+
+
+
+

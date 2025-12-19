2109fc5c1638fcff72973db7ceca9e2a65d15309
jui1960 <juifull196044@gmail.com>
2025-12-19 17:10:57 +0600

    the last level

--- The_Last_Levels.cpp
+++ The_Last_Levels.cpp
@@ -1,3 +1,30 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	// your code goes here
+// 	int t;
+// 	cin>>t;
+// 	while(t--){
+// 	    int x,y,z;
+// 	    cin>>x>>y>>z;
+// 	    int ans = x/3;
+// 	    if(x<=3)cout<<x*y<<'\n';
+// 	    else{
+// 	        if(x%3==0){
+	           
+// 	            cout<<(x*y)+(ans-1)*z<<endl;
+// 	        }else{
+	           
+// 	            cout<<(x*y)+(ans*z)<<endl;
+// 	        }
+// 	    }
+// 	}
+
+// }
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,17 +35,24 @@ int main() {
 	while(t--){
 	    int x,y,z;
 	    cin>>x>>y>>z;
-	    int ans = x/3;
-	    if(x<=3)cout<<x*y<<'\n';
+	    if(x<=3) cout<<x*y<<'\n';
 	    else{
-	        if(x%3==0){
-	           
-	            cout<<(x*y)+(ans-1)*z<<endl;
-	        }else{
-	           
-	            cout<<(x*y)+(ans*z)<<endl;
+	        int ans =x/3,rs=0,rs1=0;
+	        int mod = x%3,an = 0;
+	        if(x%3!=0){
+	            rs = ans*(3*y);
+	            rs1 = mod*y;
+	            an = rs+(ans*z)+rs1;
+	            cout<<an<<'\n';
+	            
+	        }
+	        else{
+	            int p = x*y;
+	            int anns =p+(ans-1)*z;
+	            cout<<anns<<'\n';
 	        }
 	    }
 	}
 
 }
+ 

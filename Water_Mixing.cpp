2339083d01af6e396bc090ff4a643ef94c538f04
jui1960 <juifull196044@gmail.com>
2025-12-21 21:47:09 +0600

    water_mixing

--- Water_Mixing.cpp
+++ Water_Mixing.cpp
@@ -1,3 +1,28 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	// your code goes here
+// 	int t;
+// 	cin>>t;
+// 	while(t--){
+// 	    int a,b,c,d;
+// 	    cin>>a>>b>>c>>d;
+// 	    if(a==b)cout<<"YES\n";
+// 	    else if(a>b){
+// 	        if(a-d<=b)cout<<"YES\n";
+// 	        else cout<<"NO\n";
+// 	    }
+// 	    else{
+// 	        if(a+c>=b)cout<<"YES\n";
+// 	        else cout<<"NO\n";
+// 	    }
+// 	}
+
+// }
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,16 +31,22 @@ int main() {
 	int t;
 	cin>>t;
 	while(t--){
+	    
 	    int a,b,c,d;
 	    cin>>a>>b>>c>>d;
-	    if(a==b)cout<<"YES\n";
-	    else if(a>b){
-	        if(a-d<=b)cout<<"YES\n";
-	        else cout<<"NO\n";
-	    }
+	    if(a==b) cout<<"YES\n";
 	    else{
-	        if(a+c>=b)cout<<"YES\n";
-	        else cout<<"NO\n";
+	        int ans = abs(a-b);
+	       if(a<b){
+	           if(ans<=c) cout<<"YES\n";
+	           else cout<<"NO\n";
+	           
+	       }
+	       else{
+	           if(ans>d) cout<<"NO\n";
+	           else cout<<"YES\n";
+	           
+	       }
 	    }
 	}
 

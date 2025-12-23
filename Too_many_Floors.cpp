2e621481037c5d732aa7ce906666d73adaf238ff
jui1960 <juifull196044@gmail.com>
2025-12-23 19:55:50 +0600

    max out

--- Too_many_Floors.cpp
+++ Too_many_Floors.cpp
@@ -1,23 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int x, y;
-        cin >> x >> y;
-        double chef = x / 10;
-        double chefina = y / 10;
-        if(x%10!=0) chef++;
-        if(y%10 != 0) chefina++;
-        // cout<< chef<<" "<<chefina<<'\n';
-        cout << abs(chef - chefina) << '\n';
-    }
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int ans = x/10,ans2=y/10;
+	    if(x%10!=0) ans++;
+	    if(y%10!=0) ans2++;
+	    cout<<abs(ans-ans2)<<'\n';
+	    
+	}
 
-    return 0;
 }

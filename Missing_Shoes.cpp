033e9a638355e75db1b89188801a3c23621e1106
jui1960 <juifull196044@gmail.com>
2026-01-05 18:29:53 +0600

    missing shoes

---
+++ Missing_Shoes.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int l,r;
+    cin>>l>>r;
+    cout<<abs(l-r)<<'\n';
+
+    
+
+    return 0;
+} 

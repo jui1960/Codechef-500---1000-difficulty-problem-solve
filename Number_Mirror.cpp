9f8b5dc2fbb0dd3ffc14ea8b15344a3ea287b537
jui1960 <juifull196044@gmail.com>
2026-01-06 20:27:52 +0600

    number mirro

---
+++ Number_Mirror.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a;
+    // Do not print anything before taking input
+    cin >> a;
+    cout<<a<<'\n';
+    
+    // Output the value of a in the line below
+    
+}

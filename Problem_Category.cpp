c1896185b5491305b40110461c12af87471c40d7
jui1960 <juifull196044@gmail.com>
2026-01-12 17:55:13 +0600

    prblm cetegory

---
+++ Problem_Category.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x<100) cout<<"Easy\n";
+        else if(x>=100 && x<200) cout<<"Medium\n";
+        else cout<<"Hard\n";
+    }
+
+}

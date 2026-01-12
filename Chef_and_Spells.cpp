8b2949f031b3dd10734e4b02691d9865b602d930
jui1960 <juifull196044@gmail.com>
2026-01-12 18:03:34 +0600

    chef ans_spells

---
+++ Chef_and_Spells.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        int ans = 0;
+        cout<<max({a+b,b+c,a+c})<<'\n';
+    }
+
+}

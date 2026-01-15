5791d43898aa32db846639ca453c5070f319f039
jui1960 <juifull196044@gmail.com>
2026-01-15 18:05:20 +0600

    reach_on_time

---
+++ Reach_on_Time.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int x;
+        cin>>x;
+        if(x>=30)cout<<"Yes\n";
+        else cout<<"No\n";
+    }
+   
+
+}

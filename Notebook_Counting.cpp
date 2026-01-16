803e07a728ffaea5d476ccc426d67b944d4cac18
jui1960 <juifull196044@gmail.com>
2026-01-16 20:15:00 +0600

    notebook

---
+++ Notebook_Counting.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a,b;
+    cin>>a>>b;
+    cout<<(b*100)*a<<'\n';
+    
+
+    return 0;
+}

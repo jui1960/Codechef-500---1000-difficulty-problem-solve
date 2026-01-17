0290faf5980e3d1d6721311691862f5136bb974c
jui1960 <juifull196044@gmail.com>
2026-01-17 17:22:36 +0600

    independence day

---
+++ Independence_Day.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    if(n<=15) cout<<15-n<<'\n';
+    else cout<<-1<<'\n';
+
+    return 0;
+}

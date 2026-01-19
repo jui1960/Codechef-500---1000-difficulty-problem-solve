8db9ce2f7081319386025f5d45aedbd54a001bbe
jui1960 <juifull196044@gmail.com>
2026-01-19 21:23:13 +0600

    basketball

---
+++ Christmas_Greetings.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+    if(n==25) cout<<"CHRISTMAS\n";
+    else cout<<"ORDINARY\n";
+
+    return 0;
+}

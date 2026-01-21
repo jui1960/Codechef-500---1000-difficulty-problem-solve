99473c915e81c8c2725e9ee247979cefe5057814
jui1960 <juifull196044@gmail.com>
2026-01-21 17:18:02 +0600

    is it vowel or consonant

---
+++ Is_it_a_VOWEL_or_CONSONANT.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	  char n;
+	  cin>>n;
+	  if(n=='A' || n=='E' || n=='I' || n=='O' || n=='U'){
+	      cout<<" vowel\n";
+	  }
+	  else  cout<<"Consonant\n";
+
+}

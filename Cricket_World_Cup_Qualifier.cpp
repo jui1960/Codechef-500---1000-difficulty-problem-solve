06b0c1e737945dc95948ec196c4f1d14688dbef2
jui1960 <juifull196044@gmail.com>
2026-01-21 17:24:18 +0600

    cricket world

---
+++ Cricket_World_Cup_Qualifier.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin>>n;
+    if(n>=12) cout<<"Yes\n";
+    else cout<<"No\n";
+
+    return 0;
+}

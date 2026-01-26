738523b19d80b8426f063862f0e695df644f305b
jui1960 <juifull196044@gmail.com>
2026-01-26 18:46:04 +0600

    update code

--- Second_Largest.cpp
+++ Second_Largest.cpp
@@ -10,6 +10,16 @@ int main() {
 	    cin>>a>>b>>c;
 	    int ans = a+b+c -max({a,b,c})-min({a,b,c});
 	    cout<<ans <<'\n';
+
+
+
+
+	// 	 int sum = a+b+c;
+	//    int mx = max({a,b,c});
+	//    int mn = min({a,b,c});
+	//    int ans = sum - (mn+mx);
+	//    cout<<ans<<'\n';
+	    
 	}
 
 }

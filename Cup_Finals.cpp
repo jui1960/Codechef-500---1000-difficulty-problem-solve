ec43bf68854d57021b0187e6e7e876e7a988387a
jui1960 <juifull196044@gmail.com>
2026-02-27 22:09:34 +0600

    add

--- Cup_Finals.cpp
+++ Cup_Finals.cpp
@@ -1,16 +1,39 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	// your code goes here
+// 	int t;
+// 	cin>>t;
+// 	while(t--){
+// 	    int x,y,z;
+// 	    cin>>x>>y>>z;
+// 	    int ans =abs(x-y);
+// 	    if(ans<=z)cout<<"YES\n";
+// 	    else cout<<"NO\n";
+// 	}
+
+// }
+
+
+#include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t;
-	cin>>t;
-	while(t--){
-	    int x,y,z;
-	    cin>>x>>y>>z;
-	    int ans =abs(x-y);
-	    if(ans<=z)cout<<"YES\n";
-	    else cout<<"NO\n";
-	}
+    int T;
+    cin >> T;
 
+    while(T--) {
+        int X, Y, D;
+        cin >> X >> Y >> D;
+
+        if(abs(X - Y) <= D) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
 }

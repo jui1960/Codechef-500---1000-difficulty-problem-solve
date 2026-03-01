aa41247a660c9550c09521eba891874c6735dded
jui1960 <juifull196044@gmail.com>
2026-03-01 21:58:10 +0600

    new prblm

--- A_or_B.cpp
+++ A_or_B.cpp
@@ -1,16 +1,39 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	// your code goes here
+// 	int t;
+// 	cin>>t;
+// 	while(t--){
+// 	    int x,y;
+// 	    cin>>x>>y;
+// 	    int ans =(500- (x*2)+(1000-(x+y)*4));
+// 	    int ans2=(1000-(y*4)+(500-(x+y)*2));
+// 	    cout<<max(ans,ans2)<<'\n';
+// 	}
+
+// }
+
+#include <iostream>
 using namespace std;
 
 int main() {
-	// your code goes here
-	int t;
-	cin>>t;
-	while(t--){
-	    int x,y;
-	    cin>>x>>y;
-	    int ans =(500- (x*2)+(1000-(x+y)*4));
-	    int ans2=(1000-(y*4)+(500-(x+y)*2));
-	    cout<<max(ans,ans2)<<'\n';
-	}
+    int T;
+    cin >> T;
 
+    while(T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        // Case 1: A -> B
+        int total1 = (500 - 2*X) + (1000 - 4*(X + Y));
+
+        // Case 2: B -> A
+        int total2 = (1000 - 4*Y) + (500 - 2*(X + Y));
+
+        cout << max(total1, total2) << endl;
+    }
+
+    return 0;
 }
